c8245f642d08f940ad64b2296b44431742c9cb88
Petar Palev <petarpalev2001@gmail.com>
2023-08-16 13:24:15 +0300

    ex1 solved

--- W3D1-Homework/ex1.c
+++ W3D1-Homework/ex1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <string.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 #define COUNT 5
+
 
 typedef struct Book{
     char title[150];
@@ -11,127 +13,114 @@ typedef struct Book{
     double price;
 } Book;
 
-void fill_arr(Book *arr){
-    const char POSSIBLE_CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int cmp_title_asc(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return strcmp(b1.title, b2.title);
+}
+
+int cmp_title_des(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return -(strcmp(b1.title, b2.title));
+}
+
+int cmp_author_asc(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return strcmp(b1.author, b2.author);
+}
+
+int cmp_author_des(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return -(strcmp(b1.author, b2.author));
+}
+
+int cmp_pages_asc(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return b1.pages > b2.pages;
+}
+
+int cmp_pages_des(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return !(b1.pages > b2.pages);
+}
+
+int cmp_price_asc(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return (b1.price - b2.price)*100;
+}
+
+int cmp_price_des(const void *a, const void *b){
+    Book b1 = *(Book*)a;
+    Book b2 = *(Book*)b;
+    return -(b1.price - b2.price)*100;
+}
+
+void print_elems(Book *arr){
+    for(int i = 0; i < COUNT; i++){
+        printf("Title: %s\t\tAuthor: %s\t\tPages: %d\t\tPrice:%.2lf\n", arr -> title, arr -> author, arr -> pages, arr -> price);
+        arr++;
+    }
+    printf("\n");
+}
+
+int main(void){
+
     srand(time(NULL));
+    Book *arr = malloc(sizeof(Book) * COUNT);
+    const char POSSIBLE_CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     for(int i = 0; i < COUNT; i++){
         for(int j = 0; j < 10 + rand()%10; j++){
             arr[i].title[j] = POSSIBLE_CHARS[rand()%strlen(POSSIBLE_CHARS)];
             arr[i].author[j] = POSSIBLE_CHARS[rand()%strlen(POSSIBLE_CHARS)];
         }
         arr[i].pages = 5 + rand()%1996;
-        arr[i].price = (100 + rand()%90000)/100;
+        arr[i].price = (100 + rand()%990000)/100;
     }
-}
-
-int cmp_title_asc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-
-    return b1.title[0] - b2.title[0];
-}
-
-int cmp_title_dsc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-
-    return -(b1.title[0] - b2.title[0]);
-}
-
-int cmp_author_asc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-
-    return b1.author[0] - b2.author[0];
-}
-
-int cmp_author_dsc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
     
-    return -(b1.author[0] - b2.author[0]);
-}
-
-
-int cmp_pages_asc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-    
-    return b1.pages - b2.pages;
-}
-
-int cmp_pages_dsc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-    
-    return -(b1.pages - b2.pages);
-}
-
-int cmp_price_asc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-    
-    return (b1.price > b2.price) ? 1 : -1;
-}
-
-int cmp_price_dsc(const void *a, const void *b){
-    Book b1 = *(Book*)a;
-    Book b2 = *(Book*)b;
-    
-    return (b1.price > b2.price) ? -1 : 1;
-}
-
-
-void print_elems(Book *arr){
-    for(int i = 0; i < COUNT; i++){
-        printf("Title: %s\t\tAuthod: %s\t\tPages: %d\t\tPrice:%.2lf\n", arr -> title, arr -> author, arr -> pages,arr -> price);
-        arr++;
-    }
-    printf("\n");
-}
-
-
-int main(void){
-    Book *arr = malloc(sizeof(Book) * COUNT);
-
-    fill_arr(arr);
-
+    printf("Original Array:\n");
     print_elems(arr);
 
+    printf("Sorting by title (ascending):\n");
     qsort(arr, COUNT, sizeof(Book), cmp_title_asc);
-    printf("Sorted by title (ascending):\n");
     print_elems(arr);
 
-    qsort(arr, COUNT, sizeof(Book), cmp_title_dsc);
-    printf("Sorted by title (descending):\n");
+    printf("Sorting by title (descending):\n");
+    qsort(arr, COUNT, sizeof(Book), cmp_title_des);
     print_elems(arr);
 
+    printf("Sorting by author (ascending):\n");
     qsort(arr, COUNT, sizeof(Book), cmp_author_asc);
-    printf("Sorted by author (ascending):\n");
     print_elems(arr);
 
-    qsort(arr, COUNT, sizeof(Book), cmp_author_dsc);
-    printf("Sorted by author (descending):\n");
+    printf("Sorting by author (descending):\n");
+    qsort(arr, COUNT, sizeof(Book), cmp_author_des);
     print_elems(arr);
 
+    printf("Sorting by pages (ascending):\n");
     qsort(arr, COUNT, sizeof(Book), cmp_pages_asc);
-    printf("Sorted by pages (ascending):\n");
     print_elems(arr);
 
-    qsort(arr, COUNT, sizeof(Book), cmp_pages_dsc);
-    printf("Sorted by pages (descending):\n");
+    printf("Sorting by pages (descending):\n");
+    qsort(arr, COUNT, sizeof(Book), cmp_pages_des);
     print_elems(arr);
 
+    printf("Sorting by price (ascending):\n");
     qsort(arr, COUNT, sizeof(Book), cmp_price_asc);
-    printf("Sorted by price (ascending):\n");
     print_elems(arr);
 
-    qsort(arr, COUNT, sizeof(Book), cmp_price_dsc);
-    printf("Sorted by price (descending):\n");
+    printf("Sorting by price (descending):\n");
+    qsort(arr, COUNT, sizeof(Book), cmp_price_des);
     print_elems(arr);
+
+
 
     free(arr);
-
-
     return 0;
 }
